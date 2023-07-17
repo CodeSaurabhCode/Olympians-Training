abfa5c2f62ae307c8bce7e60799b79d1af358c9a
Saurabh Kshirsagar <saurabh.kshirsagar@kongsbergdigital.com>
2023-07-17 10:35:23 +0530

    Ajax post + Dom manipualtion

---
+++ 7. 17th July (TS)/Ajax + DOM/server.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class server : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        string ajxValue = Request.Form["username"];
+        string ajxValue2 = Request.Form["usercity"];
+
+        if (ajxValue != "" )
+        {
+            Response.Write("Welcome " + ajxValue +" !" +"<br/>"+ "You live in "+ ajxValue2+"...");
+        }
+        else
+        {
+            Response.Write("Please enter a value!");
+        }
+
+        
+        Response.End(); 
+    }
+}
